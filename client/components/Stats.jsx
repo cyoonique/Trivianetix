04be4977b587d04df3f6f99a4dfe9bfa0573439f
Jie-Yun Cheng <jie-yuncheng@Jie-Yuns-MacBook-Pro.local>
2020-01-08 12:16:17 -0800

    added a leaderboard with hardcoded data on the stats page. Changed the CSS of the select dropdown menu

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
+import Table from 'react-bootstrap/Table';
 
 class Stats extends Component {
   render() {
     const questionsPosed = this.props.stats.gamesPlayed * 10;
     const questionsRight = this.props.stats.correctAnswers;
+    const PercentageRightForThisGame = this.props.correctResponses.length * 10;
     const percentageRight = questionsPosed ? Math.floor((questionsRight / questionsPosed) * 100) : 0;
     let gameMode = this.props.gameMode;
-    let scoreBoard = <p>Your All-Time Score: {percentageRight}%</p>;
+    let scoreBoard = <p>Your All-Time Score: {percentageRight}%<br/>Your Score For This Game: {PercentageRightForThisGame}%</p>;
+    console.log(`questionsPosed: ${questionsPosed}, questionsRight: ${questionsRight}, percentageRight: ${percentageRight}, PercentageRightForThisGame: ${PercentageRightForThisGame}`);
+    
+    // TODO: pull leaderBoard data from MongoDB
+    const leaderBoard = [];
+    for (let i = 1; i <= 10; i += 1) {
+      let eachLeader = (
+        <tr>
+          <td>{i}</td>
+          <td>Cat</td>
+          <td>{i * 10}</td>
+        </tr>
+      );
+      leaderBoard.push(eachLeader);
+    }
 
     return (
-      <div className='scoreboard'>{scoreBoard}</div>
+      <div>
+        <div className='scoreboard'>
+          {scoreBoard}
+        </div>
+        <div className='leaderboard'>
+          <Table striped bordered hover className='center'>
+            <thead>
+              <tr>
+                <th>Ranking</th>
+                <th>Userame     </th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {leaderBoard}
+            </tbody>
+          </Table>
+        </div>
+      </div>
     );
   }
 }
